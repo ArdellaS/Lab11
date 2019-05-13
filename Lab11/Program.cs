185b6a1d0830dc2a911ddd942336ccb37ca27003
ArdellaS <33127424+ArdellaS@users.noreply.github.com>
2019-05-13 09:49:40 -0400

    second commit

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Lab11
@@ -10,7 +10,7 @@ namespace Lab11
             string answer = null;
             do
             {
-                Console.Write("What genre would you like to see movies for?");
+                Console.Write("What genre would you like to see movies for? We have Animated, Drama, Horror, and Scifi: ");
                 var category = Console.ReadLine();
 
                 string[] horrorMovies = new string[5] { "HellRaiser", "Nightmare on Elm Street", "WishMaster", "Wrong Turn", "Candy Man" };
@@ -18,35 +18,39 @@ namespace Lab11
                 string[] dramaMovies = new string[5] { "Gone With the Wind", "Twelve Angry Men", "It's a Wonderful Life", "Five Heartbeats", "Mary, Queen if Scots" };
                 string[] scifiMovies = new string[5] { "The LawnMower Man", "Total Recall", "Twelve Monkeys", "Star Trek", "Star Wars" };
 
-                if (category == "horror")
+                if (category.Equals("horror", StringComparison.OrdinalIgnoreCase))
                 {
                     foreach (var movie in horrorMovies)
                     {
                         Console.WriteLine(movie);
                     }
                 }
-                if (category == "drama")
+                if (category.Equals("drama", StringComparison.OrdinalIgnoreCase))
                 {
                     foreach (var movie in dramaMovies)
                     {
                         Console.WriteLine(movie);
                     }
                 }
-                if (category == "animated")
+                if (category.Equals("animated", StringComparison.OrdinalIgnoreCase))
                 {
                     foreach (var movie in dramaMovies)
                     {
                         Console.WriteLine(movie);
                     }
                 }
-                if (category == "scifi")
+                if (category.Equals("scifi", StringComparison.OrdinalIgnoreCase))
                 {
                     foreach (var movie in scifiMovies)
                     {
                         Console.WriteLine(movie);
                     }
                 }
-                Console.WriteLine("Would you like to see more?");
+                else
+                {
+                    Console.WriteLine("Invalid option.");
+                }
+                Console.WriteLine("Would you like to enter another category?");
                 answer = Console.ReadLine();
             } while (answer == "y");
 
